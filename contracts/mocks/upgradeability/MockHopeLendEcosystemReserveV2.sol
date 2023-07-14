b06b8dfd2087c73e3c1672856ab387f4f8b677c7
xin.li <xin.lee.pro@gmail.com>
2023-07-14 16:58:54 +0800

    :hammer: Add mock upgradeability contract

--- contracts/mocks/upgradeability/MockHopeLendEcosystemReserveV2.sol
+++ contracts/mocks/upgradeability/MockHopeLendEcosystemReserveV2.sol
@@ -4,6 +4,8 @@ pragma solidity 0.8.17;
 import {HopeLendEcosystemReserve} from '../../treasury/HopeLendEcosystemReserve.sol';
 
 contract MockHopeLendEcosystemReserveV2 is HopeLendEcosystemReserve {
+  bool public updated = true;
+
   function getRevision() internal pure override returns (uint256) {
     return 0x2;
   }
